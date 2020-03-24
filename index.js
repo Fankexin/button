b0db0c5b35eafbb56d0f5262f713fb5a5c3abd71
范珂欣 <33643127+Fankexin@users.noreply.github.com>
2020-03-24 17:36:33 +0800

    Update index.js

--- index.js
+++ index.js
@@ -36,23 +36,3 @@ var $timerButton=(function(){
     }
     
 }());
-
-//不用 page load event
-/**
- * 封装成对象有几种方案
- * 1.简单对象字面量，不完全是面向对象，不能包括私有方法
- * var timerButton={
- *  show:function()
- * }
- * 2.工厂函数，一个函数返回值是一个简单对象
- * var timerButton=(function(){
- *  return {
- *      show:function(){}
- *  }
- * }())
- * 3.构造函数
- * function TimerButton(){
- *  
- * }
- * var tb= new TimerButton();
- */
